64cbdff8e9181893df372e5f5ab4c6efe1f21cfa
colinuponyou <colinlamprecht1@gmail.com>
2026-04-01 18:07:06 +0200

    Exceptions gekürzt

--- src/autohaus/service/exceptions.py
+++ src/autohaus/service/exceptions.py
@@ -18,24 +18,11 @@
 from collections.abc import Mapping
 
 __all__ = [
-    "EmailExistsError",
     "ForbiddenError",
     "NotFoundError",
     "UsernameExistsError",
     "VersionOutdatedError",
 ]
-
-
-class EmailExistsError(Exception):
-    """Exception, falls die Emailadresse bereits existiert."""
-
-    def __init__(self, email: str) -> None:
-        """Initialisierung von EmailExistsError mit der Emailadresse.
-
-        :param email: Bereits existierende Emailadresse
-        """
-        super().__init__(f"Existierende Email: {email}")
-        self.email = email
 
 
 class UsernameExistsError(Exception):
