8448d32e7fb8e178423924b397380e72f35fb76e
jonasriesterer <jonasriesterer23@gmail.com>
2026-04-09 21:32:17 +0200

    created sql for database

--- src/autohaus/config/resources/postgresql/create.sql
+++ src/autohaus/config/resources/postgresql/create.sql
@@ -13,68 +13,45 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
--- TEXT statt varchar(n):
--- "There is no performance difference among these three types, apart from a few extra CPU cycles
--- to check the length when storing into a length-constrained column"
--- ggf. CHECK(char_length(nachname) <= 255)
-
 -- https://www.postgresql.org/docs/current/manage-ag-tablespaces.html
-SET default_tablespace = patientspace;
+SET default_tablespace = autohausspace;
 
 -- https://www.postgresql.org/docs/current/sql-createtable.html
 -- https://www.postgresql.org/docs/current/datatype.html
--- https://www.postgresql.org/docs/current/sql-createtype.html
--- https://www.postgresql.org/docs/current/datatype-enum.html
-CREATE TYPE geschlecht AS ENUM ('MAENNLICH', 'WEIBLICH', 'DIVERS');
-CREATE TYPE familienstand AS ENUM ('LEDIG', 'VERHEIRATET', 'GESCHIEDEN', 'VERWITWET');
-CREATE TYPE facharzt AS ENUM ('CHIRURGIE', 'HALS_NASEN_OHREN', 'KARDIOLOGIE', 'NEUROLOGIE');
-
-CREATE TABLE IF NOT EXISTS patient (
-    id            INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
-    version       INTEGER NOT NULL DEFAULT 0,
-    nachname      TEXT NOT NULL,
-                  -- impliziter Index als B-Baum durch UNIQUE
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
-    email         TEXT NOT NULL UNIQUE,
-                  -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-CHECK-CONSTRAINTS
-    kategorie     INTEGER NOT NULL CHECK (kategorie >= 0 AND kategorie <= 9),
-                  -- https://www.postgresql.org/docs/current/datatype-boolean.html
-    has_newsletter BOOLEAN NOT NULL DEFAULT FALSE,
-                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
-    geburtsdatum  DATE CHECK (geburtsdatum < current_date),
-    homepage      TEXT,
-    geschlecht    geschlecht,
-    familienstand familienstand,
-                  -- https://www.postgresql.org/docs/current/datatype-json.html
-                  -- https://www.postgresql.org/docs/current/arrays.html
-                  -- fachaerzte    facharzt[],
-    fachaerzte    JSONB,
-    username      TEXT NOT NULL,
-                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
-    erzeugt       TIMESTAMP NOT NULL,
-    aktualisiert  TIMESTAMP NOT NULL
+CREATE TABLE IF NOT EXISTS autohaus (
+    id              INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
+    version         INTEGER NOT NULL DEFAULT 0,
+    name            TEXT NOT NULL,
+    anzahl_fahrzeuge INTEGER NOT NULL,
+    gruendungsdatum DATE NOT NULL,
+    homepage        TEXT,
+    telefonnummer   TEXT,
+    erzeugt         TIMESTAMP NOT NULL DEFAULT NOW(),
+    aktualisiert    TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 -- default: btree
-CREATE INDEX IF NOT EXISTS patient_nachname_idx ON patient(nachname);
+CREATE INDEX IF NOT EXISTS autohaus_name_idx ON autohaus(name);
 
 CREATE TABLE IF NOT EXISTS adresse (
     id          INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
     plz         TEXT NOT NULL CHECK (plz ~ '\d{5}'),
     ort         TEXT NOT NULL,
-    patient_id  INTEGER NOT NULL REFERENCES patient ON DELETE CASCADE
+    land        TEXT NOT NULL,
+    autohaus_id INTEGER NOT NULL REFERENCES autohaus ON DELETE CASCADE
 );
 
-CREATE INDEX IF NOT EXISTS adresse_patient_id_idx ON adresse(patient_id);
+CREATE INDEX IF NOT EXISTS adresse_autohaus_id_idx ON adresse(autohaus_id);
 CREATE INDEX IF NOT EXISTS adresse_plz_idx ON adresse(plz);
 
-CREATE TABLE IF NOT EXISTS rechnung (
-    id          INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
-                -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-NUMERIC-DECIMAL
-                -- https://www.postgresql.org/docs/current/datatype-money.html
-                -- 10 Stellen, davon 2 Nachkommastellen
-    betrag      NUMERIC(10,2) NOT NULL,
-    waehrung    TEXT NOT NULL CHECK (waehrung ~ '[A-Z]{3}'),
-    patient_id  INTEGER NOT NULL REFERENCES patient ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS auto (
+    id           INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
+    kennzeichen  TEXT NOT NULL,
+    marke        TEXT NOT NULL,
+    modell       TEXT NOT NULL,
+    baujahr      NUMERIC(4,0) NOT NULL,
+    autohaus_id  INTEGER NOT NULL REFERENCES autohaus ON DELETE CASCADE
 );
-CREATE INDEX IF NOT EXISTS rechnung_patient_id_idx ON rechnung(patient_id);
+
+CREATE INDEX IF NOT EXISTS auto_autohaus_id_idx ON auto(autohaus_id);
+CREATE INDEX IF NOT EXISTS auto_kennzeichen_idx ON auto(kennzeichen);
--- src/autohaus/config/resources/postgresql/drop_patient.sql
+++ src/autohaus/config/resources/postgresql/drop_patient.sql
@@ -13,7 +13,21 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+-- https://www.postgresql.org/docs/current/sql-dropindex.html
+DROP INDEX IF EXISTS
+    adresse_patient_id_idx,
+    adresse_plz_idx,
+    rechnung_patient_id_idx,
+    patient_nachname_idx;
+
 -- https://www.postgresql.org/docs/current/sql-droptable.html
-DROP TABLE IF EXISTS auto CASCADE;
-DROP TABLE IF EXISTS adresse CASCADE;
-DROP TABLE IF EXISTS autohaus CASCADE;
+DROP TABLE IF EXISTS
+    adresse,
+    rechnung,
+    patient;
+
+-- https://www.postgresql.org/docs/current/sql-droptype.html
+DROP TYPE IF EXISTS
+    geschlecht,
+    familienstand,
+    facharzt;
