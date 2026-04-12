b6e0e23b1e2adb20f5c4ce4f33bbbe2c3b1a21bf
jonasriesterer <jonasriesterer23@gmail.com>
2026-04-12 14:22:21 +0200

    fixed auth_router.py

--- src/autohaus/security/auth_router.py
+++ src/autohaus/security/auth_router.py
@@ -22,9 +22,9 @@ from fastapi import APIRouter, Depends, Request, Response, status
 from fastapi.responses import JSONResponse
 from loguru import logger
 
-from patient.security.dependencies import get_token_service
-from patient.security.login_data import LoginData
-from patient.security.token_service import TokenService
+from autohaus.security.dependencies import get_token_service
+from autohaus.security.login_data import LoginData
+from autohaus.security.token_service import TokenService
 
 __all__ = ["router"]
 
