a2d558c20322a322450e906a04919e29930e4d31
colinuponyou <colinlamprecht1@gmail.com>
2026-04-12 22:18:06 +0200

    Test Struktur, Unit Tests

---
+++ tests/unit/conftest.py
@@ -0,0 +1,88 @@
+# Copyright (C) 2022 - present Juergen Zimmermann, Hochschule Karlsruhe
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Fixture für pytest: Repository, Autohaus(Write)Service, KeycloakAdmin, UserService."""
+
+from keycloak import KeycloakAdmin
+from pytest import fixture
+from pytest_mock import MockerFixture
+
+from autohaus.repository import AutohausRepository
+from autohaus.security import UserService
+from autohaus.service import AutohausService, AutohausWriteService
+
+# "Fixtures" sind Funktionen, die vor den Test-Funktionen ausgefuehrt werden, um z.B.
+# wiederholt benoetigte Daten bereitzustellen (URLs, DB-Verbindungen usw.).
+# Vgl.: @BeforeEach und @BeforeAll bei JUnit
+# Ein Fixture ist eine Funktion, die i.a. in conftest.py implementiert wird und fuer
+# alle Test-Funktionen im gleichen Verzeichnis verwendet werden kann.
+# Eine Fixture-Funktion wir dann als erstes Argument an die Test-Funktion übergeben
+# oder hat das Argument autouse=True fuer die implizite Uebergabe.
+# Jedes Fixture hat einen Scope: function (= default), class, module, package, session.
+# Die Reihenfolge ist: session > package > module > class > function (~ BeforeEach).
+# module: 1x die Fixture-Funktionen pro Datei mit Test-Funktionen aufrufen
+# session: 1x die Fixture-Funktionen in diesem Verzeichnis aufrufen
+# session verwendet man fuer aufwaendige Operationen, z.B. Test-DB laden.
+
+# https://docs.pytest.org/en/stable/explanation/fixtures.html
+# https://docs.pytest.org/en/stable/how-to/fixtures.html
+# Anzeige aller fixtures ausgehend vom Wurzelverzeichnis:   uv run pytest --fixtures
+
+
+@fixture()
+def autohaus_repository() -> AutohausRepository:
+    """Fixture für AutohausRepository."""
+    return AutohausRepository()
+
+
+@fixture
+def autohaus_service(autohaus_repository: AutohausRepository) -> AutohausService:
+    """Fixture für AutohausService."""
+    return AutohausService(autohaus_repository)
+
+
+@fixture
+def keycloak_admin_mock(mocker: MockerFixture) -> KeycloakAdmin:
+    """Patching von KeycloakAdmin() innerhalb von UserService."""
+    keycloak_admin_cls_mock = mocker.patch(
+        "autohaus.security.user_service.KeycloakAdmin"
+    )
+    return keycloak_admin_cls_mock.return_value
+
+
+@fixture
+def user_service(keycloak_admin_mock) -> UserService:
+    """Patching von KeycloakAdmin.get_client_id() fuer UserService."""
+    uuid_mock = "12345678-1234-1234-1234-123456789012"
+    keycloak_admin_mock.get_client_id.return_value = uuid_mock
+    # Patching von keycloak_admin.get_client_roles()
+    autohaus_rolle_mock = {
+        "id": uuid_mock,
+        "name": "autohaus",
+        "description": "",
+        "composite": False,
+        "clientRole": True,
+        "containerId": uuid_mock,
+    }
+    keycloak_admin_mock.get_client_roles.return_value = [autohaus_rolle_mock]
+    return UserService()
+
+
+@fixture
+def autohaus_write_service(
+    autohaus_repository: AutohausRepository, user_service: UserService
+) -> AutohausWriteService:
+    """Fixture für AutohausWriteService."""
+    return AutohausWriteService(autohaus_repository, user_service)
---
+++ tests/unit/find_test.py
@@ -0,0 +1,106 @@
+# ruff: noqa: S101, D103, ARG005
+# Copyright (C) 2022 - present Juergen Zimmermann, Hochschule Karlsruhe
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Unit-Tests für find() von AutohausService."""
+
+from datetime import date
+from typing import TYPE_CHECKING
+
+from pytest import fixture, mark, raises
+
+from autohaus.entity import Adresse, Autohaus
+from autohaus.repository import Pageable
+from autohaus.service import NotFoundError
+
+if TYPE_CHECKING:
+    from pytest_mock import MockerFixture
+
+
+@fixture
+def session_mock(mocker: MockerFixture):
+    session = mocker.Mock()
+    # Patching von "with Session() as session:" in autohaus_service.py
+    mocker.patch(
+        "autohaus.service.autohaus_service.Session",
+        return_value=mocker.MagicMock(
+            __enter__=lambda self: session,
+            __exit__=lambda self, exc_type, exc, tb: None,
+        ),
+    )
+    return session
+
+
+@mark.unit
+@mark.unit_find
+def test_find_by_name(autohaus_service, session_mock) -> None:
+    # Arrange
+    name = "Mocktest Autohaus"
+    autohaus_id = 1
+    adresse_mock = Adresse(
+        id=1,
+        plz="11111",
+        ort="Mockort",
+        land="Deutschland",
+        autohaus_id=autohaus_id,
+        autohaus=None,
+    )
+    autohaus_mock = Autohaus(
+        id=autohaus_id,
+        name=name,
+        email="mock@email.test",
+        username="mocktest",
+        anzahl_fahrzeuge=50,
+        gruendungsdatum=date(2025, 1, 31),
+        homepage="https://www.test.de",
+        telefonnummer="0123456789",
+        adresse=adresse_mock,
+        autos=[],
+    )
+    adresse_mock.autohaus = autohaus_mock
+    suchparameter = {"name": name}
+    pageable = Pageable(size=5, number=0)
+    # session.scalars(select(Autohaus)...).all()
+    session_mock.scalars.return_value.all.return_value = [autohaus_mock]
+
+    # Act
+    autohaus_slice = autohaus_service.find(
+        suchparameter=suchparameter, pageable=pageable
+    )
+
+    # Assert
+    assert len(autohaus_slice.content) == 1
+    assert autohaus_slice.content[0].name == name
+
+
+@mark.unit
+@mark.unit_find
+def test_find_by_name_not_found(autohaus_service, session_mock) -> None:
+    # Arrange
+    name = "Notfound Autohaus"
+    suchparameter = {"name": name}
+    pageable = Pageable(size=5, number=0)
+    # session.scalars(select(Autohaus)...).all()
+    session_mock.scalars.return_value.all.return_value = []
+
+    # Act
+    with raises(NotFoundError) as err:
+        autohaus_service.find(suchparameter=suchparameter, pageable=pageable)
+
+    # Assert
+    assert err.type == NotFoundError
+    assert str(err.value) == "Not Found"  # super().__init__("Not Found")
+    assert err.value.suchparameter is not None
+    assert err.value.suchparameter.get("name") == name  # pyright: ignore[reportOptionalMemberAccess]
